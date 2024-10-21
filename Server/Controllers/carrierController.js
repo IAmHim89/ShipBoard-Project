6bf69a1a65a09bcd422da0b88c418fbae63eef60
IAmHim89 <computertech0418@gmail.com>
2024-10-21 18:29:14 -0400

    updated comments on server

--- Server/Controllers/carrierController.js
+++ Server/Controllers/carrierController.js
@@ -7,26 +7,26 @@ const Carrier = require("../Models/carrierModel");
 router.post("/signup", async (req, res) => {
   try {
     const newCarrier = await Carrier.create({
-      //create new carrier Info for signup
+      //!create new carrier Info for signup
       businessName: req.body.businessName,
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       dotNumber: req.body.dotNumber,
       email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 12), //password hashed and sync'd
+      password: bcrypt.hashSync(req.body.password, 12), //!password hashed and sync'd
     });
-    //Token variable to hold token generation
+    //!Token variable to hold token generation
     const token = jwt.sign({ id: newCarrier._id }, process.env.JWT_SECRET, {
       expiresIn: "2days", //token expiration
     });
     res.status(200).json({
-      Created: newCarrier, //new carrier info created
-      Msg: "Success, Carrier Created!", //Message for carrier created
+      Created: newCarrier, //!new carrier info created
+      Msg: "Success, Carrier Created!", //!Message for carrier created
       Token: token,
     });
   } catch (err) {
     res.status(500).json({
-      //conditional error call to check the same carrier wasnt made twice
+      //!conditional error call to check the same carrier wasnt made twice
       Error: err.code === 11000 ? "Admin already exists" : err.message,
     });
   }
@@ -34,24 +34,24 @@ router.post("/signup", async (req, res) => {
 
 router.post("/signin", async (req, res) => {
   try {
-    const { email, password } = req.body; //email and password required
+    const { email, password } = req.body; //!email and password required
 
-    const carrier = await Carrier.findOne({ email: email }); //carrier email key and value
+    const carrier = await Carrier.findOne({ email: email }); //!carrier email key and value
 
-    if (!carrier) throw new Error("Incorrect Admin name or password"); //conditional error call for email required
+    if (!carrier) throw new Error("Incorrect Admin name or password"); //!conditional error call for email required
 
-    const passwordMatch = await bcrypt.compare(password, carrier.password); //password required through carrier
+    const passwordMatch = await bcrypt.compare(password, carrier.password); //!password required through carrier
 
-    if (!passwordMatch) throw new Error("Password is Incorrect!"); // conditional error call for password required
+    if (!passwordMatch) throw new Error("Password is Incorrect!"); //! conditional error call for password required
 
-    //generate a token if the log in is successful
+    //!generate a token if the login is successful
     const token = jwt.sign({ id: carrier._id }, process.env.JWT_SECRET, {
       expiresIn: "1day",
     });
     res.status(200).json({
-      Msg: "You have successfully logged In!", // message for succesful login
-      Carrier: carrier, //return carrier info
-      Token: token, //return token
+      Msg: "You have successfully logged In!", //! message for succesful login
+      Carrier: carrier, //!return carrier info
+      Token: token, //!return token
     });
   } catch (err) {
     res.status(500).json({
